2d52ecc0d16c95bb8fd551ab4ab8c60acd7b8275
Sigma142 <Christian.lp06@gmail.com>
2025-10-09 10:19:25 +0200

    Sigma add Kid
    simga

---
+++ Spill projekt/Assets/This is how we move (NIGGA).cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Thisishowwemove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * vertical + right * horizontal;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+}
