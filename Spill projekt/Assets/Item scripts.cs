ffaa1d094930ebf7b68a705867fa8b140998e72b
Sigma142 <Christian.lp06@gmail.com>
2025-10-23 11:34:26 +0200

    sitten

---
+++ Spill projekt/Assets/Item scripts.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
+public class Itemscripts : MonoBehaviour
+{
+    public string itemName;
+    public Sprite icon;
+    public int id;
+    public bool isStackable;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
